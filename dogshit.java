171d094661656138d639e991bdd619c6ca72e91c
DrTarrasch <66882649+RaccAttacks@users.noreply.github.com>
2020-10-10 10:28:34 -0400

    Create dogshit.java

---
+++ dogshit.java
@@ -0,0 +1,5 @@
+import tester.*;                // The tester library
+import javalib.worldimages.*;   // images, like RectangleImage or OverlayImages
+import javalib.funworld.*;      // the abstract World class and the big-bang library
+import java.awt.Color;          // general colors (as triples of red,green,blue values)
+                                // and predefined colors (Red, Green, Yellow, Blue, Black, White)
