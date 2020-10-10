5086ea1ef695e5bfa9d1c11462e373c191e3f7ae
DrTarrasch <66882649+RaccAttacks@users.noreply.github.com>
2020-10-10 10:49:27 -0400

    Update and rename dogshit.java to Game.java

--- Game.java
+++ Game.java
@@ -3,3 +3,12 @@ import javalib.worldimages.*;   // images, like RectangleImage or OverlayImages
 import javalib.funworld.*;      // the abstract World class and the big-bang library
 import java.awt.Color;          // general colors (as triples of red,green,blue values)
                                 // and predefined colors (Red, Green, Yellow, Blue, Black, White)
+
+class Location {
+  int x; // x-coordinate
+  int y; // y-coordinate
+}
+
+class EnemyFish {
+  
+}
